f5f90e3600083662e98a8c3b8f8e3c6283dc4595
tsubauaaa <tsubasa1173@gmail.com>
2019-04-28 23:05:16 +0900

    確率的勾配降下法を追加

--- mean_squared_error.py
+++ mean_squared_error.py
@@ -1,25 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 学習データを行列として読み込む
+# 学習データを読み込む
 train = np.loadtxt('click.csv', delimiter=',', skiprows=1)
-
-# 1列目の抽出
 train_x = train[:,0]
-# 2列目の抽出
 train_y = train[:,1]
-
-# 標準化
-def standardize(x):
-    mu = train_x.mean()
-    sigma = train_x.std()
-    return (x - mu) / sigma
-
-# 1列目の標準化
-train_z = standardize(train_x)
-
-# 学習率
-ETA = 1e-3
 
 # 平均二乗誤差
 def MSE(x, y):
@@ -31,20 +16,30 @@ theta = np.random.rand(3)
 # 平均二乗誤差の履歴
 errors = []
 
-# 学習データの[1, x, x^2]の行列を作る
+# 学習データの行列を作る
 def to_matrix(x):
     return np.vstack([np.ones(x.shape[0]), x, x ** 2]).T
+
+# 予測関数
+def f(x):
+    return np.dot(x, theta)
 
 # 目的関数
 def E(x, y):
     return 0.5 * np.sum((y - f(x)) ** 2)
 
-# 予測関数
-# f_theta(x) = X.theta
-def f(x):
-    return np.dot(x, theta)
+# 標準化
+mu = train_x.mean()
+sigma = train_x.std()
+def standardize(x):
+    return (x -mu) / sigma
+
+train_z = standardize(train_x)
 
 X = to_matrix(train_z)
+
+# 学習率
+ETA = 1e-3
 
 # 誤差の差分
 diff = 1
@@ -52,14 +47,11 @@ diff = 1
 # 学習を繰り返す
 errors.append(MSE(X, train_y))
 while diff > 1e-2:
-    # パラメータを更新
     theta = theta - ETA * np.dot(f(X) - train_y, X)
-    # 前回の誤差との差分を計算
     errors.append(MSE(X, train_y))
     diff = errors[-2] - errors[-1]
 
 # 誤差をプロット
 x = np.arange(len(errors))
-
 plt.plot(x, errors)
 plt.show()
--- regression_linear_sgd.py
+++ regression_linear_sgd.py
@@ -1,35 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 学習データを行列として読み込む
+# 学習データを読み込む
 train = np.loadtxt('click.csv', delimiter=',', skiprows=1)
-
-# 1列目を抽出して行ベクトルにする
 train_x = train[:,0]
-# 2列目を抽出して行ベクトルにする
 train_y = train[:,1]
 
-# 標準化
-def standardize(x):
-    mu = train_x.mean()
-    sigma = train_x.std()
-    return (x - mu) / sigma
-
-# 1列目の標準化
-train_z = standardize(train_x)
-
-# 学習率
-ETA = 1e-3
+# 平均二乗誤差
+def MSE(x, y):
+    return (1 / x.shape[0]) * np.sum((y - f(x)) ** 2)
 
 # パラメータをランダムに初期化
 theta = np.random.rand(3)
 
-# 学習データの[1, x, x^2]の行列を作る
+# 平均二乗誤差の履歴
+errors = []
+
+# 学習データの行列を作る
 def to_matrix(x):
     return np.vstack([np.ones(x.shape[0]), x, x ** 2]).T
 
 # 予測関数
-# f_theta(x) = X.theta
 def f(x):
     return np.dot(x, theta)
 
@@ -37,24 +28,28 @@ def f(x):
 def E(x, y):
     return 0.5 * np.sum((y - f(x)) ** 2)
 
+# 標準化
+mu = train_x.mean()
+sigma = train_x.std()
+def standardize(x):
+    return (x -mu) / sigma
+
+train_z = standardize(train_x)
+
 X = to_matrix(train_z)
 
-# 平均二乗誤差
-def MSE(x, y):
-    return (1 / x.shape[0]) * np.sum((y - f(x)) ** 2)
+# 学習率
+ETA = 1e-3
 
 # 誤差の差分
 diff = 1
 
-# 平均二乗誤差の履歴
-errors = []
-
 # 学習を繰り返す
 errors.append(MSE(X, train_y))
 while diff > 1e-2:
-    # 学習データを並べ替えるためランダムな順列を用意する
+    # 学習データを並べ替えるためにランダムな順列を用意する
     p = np.random.permutation(X.shape[0])
-    # 学習データをランダムに取り出して確率的勾配降下法でパラメータを更新
+    # 学習データをランダムに取り出して確率的勾配降下法でパラメータ更新
     for x, y in zip(X[p,:], train_y[p]):
         theta = theta - ETA * (f(x) - y) * x
     # 前回の誤差との差分を計算
